8e1dec3a74a7ffe52f7d977124806a921d55436f
Keki <cristian.velez7@gmail.com>
2019-11-30 02:57:03 -0600

    Added bgm

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMusic : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        CreateSingleton();
+    }
+
+
+    #region Private Members
+
+    private void CreateSingleton()
+    { 
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    #endregion
+}
