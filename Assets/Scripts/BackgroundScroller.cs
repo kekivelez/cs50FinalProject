d6102f32d3d253f61cc4692da1d934eb2a81b59d
Keki <cristian.velez7@gmail.com>
2019-11-29 19:53:43 -0600

    Replaced static background with scrolling background and added second shredder for enemy projectiles

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    #region Config Parameters
+    [SerializeField] private float _scrollSpeed = 0.02f;
+
+    #endregion
+
+    #region Instance Variables
+    private Material _backgroundMaterial;
+    private Vector2 _scrollOffset;
+
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backgroundMaterial = GetComponent<Renderer>().material;
+        _scrollOffset = new Vector2(0, _scrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _backgroundMaterial.mainTextureOffset += _scrollOffset * Time.deltaTime;
+    }
+}
