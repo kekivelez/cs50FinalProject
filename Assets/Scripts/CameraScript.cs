7182f3b012f3f0ea996dfefc6d974bd764ec533e
Keki <cristian.velez7@gmail.com>
2019-12-07 21:54:38 -0600

    Fixed resolution and scaling to 9:16 aspect ratio

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private float lastWidth;
+    private float lastHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1080, 1920, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
